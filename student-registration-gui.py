5f8457021451987400b810a83abe035ad7b1aee3
Bhoomi <shallowlane892@gmail.com>
2026-07-23 15:26:41 +0530

    update

--- student-registration-gui.py
+++ student-registration-gui.py
@@ -3,5 +3,31 @@ window=tk.Tk()
 window.title("student registration form")
 window.geometry("500x700")
 
+label=tk.Label(
+    window,
+    text="Student Registration Form",
+    font=("Montserrat",19),
+    #justify="center"
+    )
+label.pack()
+
+fill_frame=tk.Frame(window)
+fill_frame.pack()
+
+name=tk.Label(
+    fill_frame,
+    text="Name: ",
+    font=("arial",14),
+    justify="left"
+)
+name.grid(row=0, column=0)
+
+name=tk.Entry(
+    fill_frame,
+    font=("arial",14),
+    justify="right"
+)
+name.grid(row=0, column=1)
+
 
 window.mainloop()
