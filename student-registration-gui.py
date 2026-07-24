55c3eae2c016b34a9ef0f7b9bfb205ad6fb43c9e
Bhoomi <shallowlane892@gmail.com>
2026-07-24 20:24:27 +0530

    update

--- student-registration-gui.py
+++ student-registration-gui.py
@@ -14,20 +14,19 @@ label.pack()
 fill_frame=tk.Frame(window)
 fill_frame.pack()
 
-name=tk.Label(
+name_label=tk.Label(
     fill_frame,
     text="Name: ",
+    font=("arial",14)
+)
+name_label.grid(row=0, column=0, padx=10)
+
+name_entry=tk.Entry(
+    fill_frame,
     font=("arial",14),
     justify="left"
 )
-name.grid(row=0, column=0)
-
-name=tk.Entry(
-    fill_frame,
-    font=("arial",14),
-    justify="right"
-)
-name.grid(row=0, column=1)
+name_entry.grid(row=0, column=1, padx=10 )
 
 
 window.mainloop()
