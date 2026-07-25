908dc318169010ad2d8052e74a3a51489211f0b6
Bhoomi <shallowlane892@gmail.com>
2026-07-25 20:46:13 +0530

    update

--- student-registration-gui.py
+++ student-registration-gui.py
@@ -2,6 +2,7 @@ import tkinter as tk
 window=tk.Tk()
 window.title("student registration form")
 window.geometry("500x700")
+window.configure(bg="pink")
 
 label=tk.Label(
     window,
@@ -9,24 +10,39 @@ label=tk.Label(
     font=("Montserrat",19),
     #justify="center"
     )
-label.pack()
+label.pack(pady=30)
 
-fill_frame=tk.Frame(window)
+fill_frame=tk.Frame(
+    window
+    )
 fill_frame.pack()
 
 name_label=tk.Label(
     fill_frame,
     text="Name: ",
-    font=("arial",14)
+    font=("Poppins",14)
 )
-name_label.grid(row=0, column=0, padx=10)
+name_label.grid(row=0, column=0, padx=(0,50))
 
 name_entry=tk.Entry(
     fill_frame,
-    font=("arial",14),
-    justify="left"
+    font=("Poppins",14),
 )
-name_entry.grid(row=0, column=1, padx=10 )
+name_entry.grid(row=0, column=1 )
+
+roll_label=tk.Label(
+    fill_frame,
+    text="Roll no: ",
+    font=("Poppins",14)
+)
+roll_label.grid(row=1, column=0, padx=(0,50))
+
+roll_entry=tk.Entry(
+    fill_frame,
+    font=("Poppins",14)
+)
+roll_entry.grid(row=1, column=1,)
+
 
 
 window.mainloop()
