3bf942b5560f9aef33481261848a4976e5167c74
Bhoomi <shallowlane892@gmail.com>
2026-07-27 19:20:50 +0530

    update

--- student-registration-gui.py
+++ student-registration-gui.py
@@ -10,8 +10,35 @@ label=tk.Label(
     text="Student Registration Form",
     font=("Montserrat",19),
     #justify="center"
+    bg="pink"
     )
 label.pack(pady=30)
+
+def register():
+    name=name_entry.get()
+    roll=roll_entry.get()
+    age=age_entry.get()
+    course=course_menu.get()
+    gnd=gender.get()
+    address=address_txt.get("1.0",tk.END)
+
+    skills=[]
+    if python_var.get():
+        skills.append("python")
+    if html_var.get():
+        skills.append("html")
+    if css_var.get():
+        skills.append("css")
+    if java_var.get():
+        skills.append("java")
+
+    print("Name:", name)
+    print("Roll:", roll)
+    print("Age:", age)
+    print("Course:", course)
+    print("Gender:", gnd)
+    print("skills:",skills)
+    print("Address:", address)
 
 fill_frame=tk.Frame(
     window
@@ -23,7 +50,7 @@ name_label=tk.Label(
     text="Name: ",
     font=("Poppins",14)
 )
-name_label.grid(row=0, column=0, padx=(0,50))
+name_label.grid(row=0, column=0, padx=(0,50),pady=4)
 
 name_entry=tk.Entry(
     fill_frame,
@@ -36,7 +63,7 @@ roll_label=tk.Label(
     text="Roll no: ",
     font=("Poppins",14)
 )
-roll_label.grid(row=1, column=0, padx=(0,50))
+roll_label.grid(row=1, column=0, padx=(0,50),pady=4)
 
 roll_entry=tk.Entry(
     fill_frame,
@@ -47,9 +74,10 @@ roll_entry.grid(row=1, column=1,)
 age_label=tk.Label(
     fill_frame,
     text="age: ",
-    font=("poppins",14)
+    font=("poppins",14),
+    justify="left"
 )
-age_label.grid(row=2, column=0, padx=(0,50))
+age_label.grid(row=2, column=0, padx=(0,50),pady=4)
 
 age_entry=tk.Entry(
     fill_frame,
@@ -62,7 +90,7 @@ course_label=tk.Label(
     text="course:",
     font=("poppins",14)
 )
-course_label.grid(row=3, column=0, padx=(0,50))
+course_label.grid(row=3, column=0, padx=(0,50),pady=4)
 
 course_menu=ttk.Combobox(
     fill_frame,
@@ -76,7 +104,7 @@ gender_label=tk.Label(
     text="gender: ",
     font=("poppins",14)
 )
-gender_label.grid(row=4, column=0, padx=(0,50))
+gender_label.grid(row=4, column=0, padx=(0,50),pady=4)
 
 gender=tk.StringVar()
 
@@ -114,17 +142,17 @@ skill_label=tk.Label(
     text="skills: ",
     font=("poppins",14)
 )
-skill_label.grid(row=5,column=0,padx=(0,50))
+skill_label.grid(row=5,column=0,padx=(0,50),pady=4)
 
 skill_frame=tk.Frame(
     fill_frame
 )
 skill_frame.grid(row=5,column=1)
 
-python_var=tk.BooleanVar
-html_var=tk.BooleanVar
-css_var=tk.BooleanVar
-java_var=tk.BooleanVar
+python_var=tk.BooleanVar()
+html_var=tk.BooleanVar()
+css_var=tk.BooleanVar()
+java_var=tk.BooleanVar()
 
 python_cb=tk.Checkbutton(
     skill_frame,
@@ -144,7 +172,7 @@ html_cb.grid(row=0,column=1)
 
 css_cb=tk.Checkbutton(
     skill_frame,
-    text="something something",
+    text="css",
     variable=css_var
 )
 css_cb.grid(row=0,column=2)
@@ -161,7 +189,7 @@ address_label=tk.Label(
     text="Address: ",
     font=("poppins",14)
 )
-address_label.grid(row=6,column=0,padx=(0,50))
+address_label.grid(row=6,column=0,padx=(0,50),pady=4)
 
 address_txt=tk.Text(
     fill_frame,
@@ -169,9 +197,16 @@ address_txt=tk.Text(
     width=30,
     height=4
 )
-address_txt.grid(row=6,column=1)
+address_txt.grid(row=6,column=1,padx=10,pady=10)
 
-
-
+register_btn=tk.Button(
+    window,
+    text="Register",
+    height=3,
+    width=10,
+    font=(12),
+    command=register
+)
+register_btn.pack(pady=20)
 
 window.mainloop()
