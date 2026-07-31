619aaf90ee6caa4cbb8e4cd58af7ec2e3c0fc691
Bhoomi <shallowlane892@gmail.com>
2026-07-31 15:31:58 +0530

    update

--- student-registration-gui.py
+++ student-registration-gui.py
@@ -218,6 +218,11 @@ class studentRegistration:
         print("skills:",skills)
         print("Address:",address)
 
+        messagebox.showinfo(
+            "success",
+            "registration successful"
+        )
+
 window=tk.Tk()
 window.title("student registration form")
 window.geometry("500x700")
